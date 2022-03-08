ed3602957914d05bc28e158aaef0140a26f321e1
Castor Velasquez <castor.v.2013@gmail.com>
2022-03-08 16:53:12 -0500

    restoring head

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require("express")
 const path = require("path")
-const mongoose = require("mongoose")
 
-
-const server = http.createServer(app)
-const {Server} = require("socket.io")
-const io = new Server(server)
 
 const app = express();
-<<<<<<< HEAD
 app.use(express.static('public'))
-=======
-app.use(express.static("public"))
->>>>>>> 809258a5683d658d19595fd85648dc6ad59e2c24
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
